e5aebe115b8708ecdd915be7dd4be5137b3e0bd8
alxdrr <lexlex277@gmail.com>
2024-04-04 22:21:08 +0700

    fix: footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
   return (
     <div className="w-screen bg-primary">
       <div className="flex flex-col justify-center items-center h-max  py-16 px-6 max-w-screen-xl w-full mx-auto text-white gap-8">
-        <main className="flex w-full justify-between lg:gap-6 xl:gap-9">
+        <main className="flex flex-col sm:flex-row w-full justify-between gap-8 lg:gap-6 xl:gap-9">
           <div className="flex flex-col">
-            <section className="text-secondary lg:text-lg xl:text-xl font-black mb-4">Follow Us</section>
-            <section className="flex gap-4">
+            <section className="text-secondary lg:text-base xl:text-xl font-black mb-2 xl:mb-4">Follow Us</section>
+            <section className="flex gap-2 xl:gap-4">
               <a href="">
                 <img src={logo1} />
               </a>
@@ -19,47 +19,57 @@ const Footer = () => {
               </a>
             </section>
           </div>
-          <div className="flex flex-col gap-4">
-            <section className="text-secondary lg:text-lg xl:text-xl font-black">Explore</section>
-            <a className="text-white xl:text-base lg:text-sm font-normal" href="/">
+          <div className="flex flex-col gap-2 xl:gap-4">
+            <section className="text-secondary lg:text-base xl:text-xl font-black">Explore</section>
+            <a className="text-white sm:text-xs xl:text-base lg:text-sm font-normal" href="/">
               About Us
             </a>
-            <a className="text-white xl:text-base lg:text-sm font-normal" href="/">
+            <a className="text-white sm:text-xs xl:text-base lg:text-sm font-normal" href="/">
               Services
             </a>
-            <a className="text-white xl:text-base lg:text-sm font-normal" href="/">
+            <a className="text-white sm:text-xs xl:text-base lg:text-sm font-normal" href="/">
               FAQ
             </a>
           </div>
-          <div className="flex flex-col gap-4">
-            <section className="text-secondary lg:text-lg xl:text-xl font-black">Contact Us</section>
-            <a className="text-white xl:text-base lg:text-sm font-normal" href="/">
+          <div className="flex flex-col gap-2 xl:gap-4">
+            <section className="text-secondary lg:text-base xl:text-xl font-black">Contact Us</section>
+            <a className="text-white sm:text-xs xl:text-base lg:text-sm font-normal" href="/">
               +62 821-3875-4680
             </a>
-            <a className="text-white xl:text-base lg:text-sm font-normal" href="/">
+            <a className="text-white sm:text-xs xl:text-base lg:text-sm font-normal" href="/">
               forcomstechnology@gmail.com
             </a>
           </div>
           <div className="flex flex-col">
-            <section className="text-secondary font-black lg:text-lg xl:text-xl mb-4">Address</section>
-            <p className="text-white xl:text-base lg:text-sm font-normal lg:max-w-40 xl:max-w-xs" href="/">
+            <section className="text-secondary font-black lg:text-base xl:text-xl mb-2 xl:mb-4">Address</section>
+            <p className="text-white sm:text-xs xl:text-base lg:text-sm font-normal md:max-w-60 sm:max-w-40 xl:max-w-xs" href="/">
               Jl. Ir. Sutami, Kemenuh, Kec. Sukawati, Kabupaten Gianyar,  Bali 80582
             </p>
             <a href="/" className="text-gray-400">
               View on maps
             </a>
           </div>
-          <div className="flex flex-col ">
+          <div className="hidden lg:flex flex-col ">
             <a href="https://forcoms.com" className="w-full">
               <img src={forcomsLogo} alt="forcoms logo" className="w-full h-full max-w-[220px] mx-auto" />
             </a>
-            <p className="text-gray-300 italic font-normal text-center lg:text-lg xl:text-xl mt-8" href="/">
+            <p className="text-gray-300 italic font-normal text-center lg:text-base xl:text-xl mt-8" href="/">
               Innovate for Growth
             </p>
           </div>
         </main>
+        {/* Tablet Footer */}
+        <div className="flex lg:hidden w-full flex-col ">
+          <a href="https://forcoms.com" className="w-full">
+            <img src={forcomsLogo} alt="forcoms logo" className="w-full h-full max-w-[280px] mx-auto" />
+          </a>
+          <p className="text-gray-300 italic font-normal text-center text-xl mt-4" href="/">
+            Innovate for Growth
+          </p>
+        </div>
+
         <hr className="w-full border-gray-300" />
-        <p className="text-white xl:text-base lg:text-sm font-normal max-w-xs" href="/">
+        <p className="text-white sm:text-xs xl:text-base lg:text-sm font-normal max-w-xs" href="/">
           © 2024 Forcoms, All Rights Reserved.
         </p>
       </div>
